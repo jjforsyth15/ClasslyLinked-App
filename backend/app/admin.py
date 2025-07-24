341d5b02df456365844e538b9fd041e283dfed07
jjforsyth15 <jjforsyth15@gmail.com>
2025-07-23 22:55:33 -0700

    To admin.py: # Added:         #   CreateCourse - Add new course to COURSES         #   RemoveStudentFrom_course - Remove student from a course         #   RemoveStudentFrom_STUDENTS - Remove student from STUDENTS         #   PrintCourses - Print list of courses         #   PrintStudentsIn_STUDENTS -Print list of students         #   PromoteAdmin - Promote student to admin         #   PrintStudentsIn_course - List students in a course

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -14,7 +14,7 @@ class Admin(Student):
         super().__init__(first, last, username, password, isAdmin=True)
 
 
-    def createCourse(self, c_name, c_num):
+    def CreateCourse(self, c_name, c_num):
         
         course = courses.find_one({"courseNumber": c_num})
 
@@ -26,7 +26,7 @@ class Admin(Student):
             print(f"Course '{c_name}' ({c_num}) created.")
             return True
 
-    def removeStudentFrom_course(self, studentToRemove, courseToRemoveFrom):
+    def RemoveStudentFrom_course(self, studentToRemove, courseToRemoveFrom):
         student = students.find_one({"userName":studentToRemove})
         course = courses.find_one({"courseNumber": courseToRemoveFrom})
 
@@ -45,17 +45,67 @@ class Admin(Student):
             )
             return True
 
-    
+    def RemoveStudentFrom_STUDENTS(self, studentToRemove):
+
+        student = students.find_one({"userName": studentToRemove})
+
+        if student is None:
+            print("Student does not exist")
+            return False
+        else:
+            students.delete_one({"userName": studentToRemove})
+            courses.update_many({}, {"$pull": {"students": studentToRemove}})
+            return True
         
 
+    def PrintCourses(self):
+        for i, course in enumerate(courses.find(), 1):
+            enrolled = len(course.get("students", []))
+            print(i, ". Course: ", course["courseName"], " | Number: ", course["courseNumber"], " | Number of Students: ", enrolled)
+
+
+    def PrintStudentsIn_STUDENTS(self):
+        for i, student in enumerate(students.find(), 1):
+            num_courses = len(student.get("courses", []))
+            
+            if student["isAdmin"] is True:
+                print(i, ". Name: ", student["firstName"], " ", student["lastName"], " | Username:", student["userName"], " | Admin")
+            else:
+                print(i, ". Name: ", student["firstName"], " ", student["lastName"], " | Username:", student["userName"])
+
+    def PromoteToAdmin(self, student_to_promote):
+        admin = students.find_one({"userName": student_to_promote})
+
+        if admin["isAdmin"] is True:
+            print("Student is already admin")
+            return False
+        else:
+            students.update_one(
+                {"userName": student_to_promote},
+                {"$set": {"isAdmin": True}}
+                )
+            return True
+        
+    def PrintStudentsIn_course(self, course_to_check):
+        course = courses.find_one({"courseNumber": course_to_check})
+        
+        for i, username in enumerate(course["students"], 1):
+            student = students.find_one({"userName": username})
+            name = student["firstName"] + " " + student["lastName"]
+            
+            if student["isAdmin"] is True:
+                print(i, ". Name: ", name, " | Username: ", username, " | Admin")
+            else:
+                print(i, ". Name: ", name, " | Username: ", username)
+            
+
     # methods to create:
-    
-    #   Remove student from STUDENTS
-    #   Print list of courses
-    #   Print list of students
-    #   Promote student to admin
-    #   List students in a course
 
     # Added:
-        #   Add new course to COURSES
-        #   Remove student from a course
+        #   CreateCourse - Add new course to COURSES
+        #   RemoveStudentFrom_course - Remove student from a course
+        #   RemoveStudentFrom_STUDENTS - Remove student from STUDENTS
+        #   PrintCourses - Print list of courses
+        #   PrintStudentsIn_STUDENTS -Print list of students
+        #   PromoteAdmin - Promote student to admin
+        #   PrintStudentsIn_course - List students in a course
