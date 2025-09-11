6e4f9f8c6c5a67ec98fe2faeb17f01aa6f475352
jjforsyth15 <jjforsyth15@gmail.com>
2025-09-11 00:24:11 -0700

    Started on test file for mateRequest.

---
+++ backend/app/mateRequestTest.py
@@ -0,0 +1,17 @@
+from mateRequest import mateRequest
+
+def mateRequestTest():
+    
+    user1 = input("Enter username of request sender: ")
+
+    user2 = input("Enter the username of the receiver: ")
+
+    request = mateRequest(user1, user2)
+
+    
+
+    
+
+
+
+mateRequestTest()
